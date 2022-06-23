bde7d0149b0ed71cf406d88f3fcc15457437204f
jtaraya <jacobtaraya@gmail.com>
2022-06-24 00:28:49 +0300

    create three packages. utilities, models and services to group files

--- src/main/java/com/jtaraya/Main.java
+++ src/main/java/com/jtaraya/Main.java
@@ -1,6 +1,7 @@
 package com.jtaraya;
 
 public class Main {
+    // create three packages. utilities, models and services to group files.
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/com/jtaraya/util/Validator.java
@@ -0,0 +1,15 @@
+package com.jtaraya.util;
+
+public class Validator {
+    // have three methods.
+    // note with public access modifiers
+    public boolean operationValidator(){
+        return true;
+    }
+        public boolean messageValidator(){
+            return true;
+        }
+            public boolean keyValidator(){
+                return true;
+            }
+}
