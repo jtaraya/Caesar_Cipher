485618bae1865f4b4c24b786ddf7f03ec0ba1226
jtaraya <jacobtaraya@gmail.com>
2022-06-24 00:59:47 +0300

    adding getters and setters to access 'private' modifiers

--- src/main/java/com/jtaraya/models/UserInput.java
+++ src/main/java/com/jtaraya/models/UserInput.java
@@ -4,6 +4,37 @@ package com.jtaraya.models;
 //for any fields we need to access PRIVATE and then use getters and setters to access
 public class UserInput {
     private String operation;
+
     private String message;
+    
     private int key;
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+
+
+
+
 }
