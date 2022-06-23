a1235ac6b96f432ad45ddc5c3bc1dbf824f9e85b
jtaraya <jacobtaraya@gmail.com>
2022-06-24 00:49:53 +0300

    in models create three Strings in private access modifier

---
+++ src/main/java/com/jtaraya/models/UserInput.java
@@ -0,0 +1,9 @@
+package com.jtaraya.models;
+
+//create three fields in String and
+//for any fields we need to access PRIVATE and then use getters and setters to access
+public class UserInput {
+    private String operation;
+    private String message;
+    private int key;
+}
