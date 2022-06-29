43eea9b20cd7e3be89c5a38a95c8cae4d5ea9fbf
jtaraya <jacobtaraya@gmail.com>
2022-06-29 09:08:52 +0300

    class

--- src/main/java/com/jtaraya/util/Validator.java
+++ src/main/java/com/jtaraya/util/Validator.java
@@ -3,13 +3,18 @@ package com.jtaraya.util;
 public class Validator {
     // have three methods.
     // note with public access modifiers
-    public boolean operationValidator(){
+    public boolean operationValidator(String operation){
         return true;
+        //checks if operation is encode or decode.
+        // if its valid  - operation = encode
+        String encode = "encode";
+        boolean isValid = operation.equalsIgnoreCase( String:"encode")
     }
-        public boolean messageValidator(){
+        public boolean messageValidator(String message){
             return true;
+            //to check that its not empty for message
         }
-            public boolean keyValidator(){
+            public boolean keyValidator(int key){
                 return true;
             }
 }
