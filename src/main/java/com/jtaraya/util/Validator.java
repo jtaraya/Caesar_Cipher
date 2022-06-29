5001a57d4a80c36c9aca1d8730b6a4a1d3049358
jtaraya <jacobtaraya@gmail.com>
2022-06-29 09:42:12 +0300

    change method from public to private

--- src/main/java/com/jtaraya/util/Validator.java
+++ src/main/java/com/jtaraya/util/Validator.java
@@ -3,18 +3,18 @@ package com.jtaraya.util;
 public class Validator {
     // have three methods.
     // note with public access modifiers
-    public boolean operationValidator(String operation){
+    private boolean operationValidator(String operation){
 
         //checks if operation is encode or decode.
         // if its valid  - operation = encode
         return operation.equalsIgnoreCase(String:"encode") ||
                 operation.equalsIgnoreCase(String: "decode" );
     }
-        public boolean messageValidator(String message){
+        private boolean messageValidator(String message){
             return ! (message.isBlank() && message.isEmpty());
             //to check that it's not empty for message
         }
-        public boolean keyValidator(int key) {
+        private boolean keyValidator(int key) {
             return(key >= 1) && (key <= 25);
         }
 }
