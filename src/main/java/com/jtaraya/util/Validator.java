a0fd13ee7f6e769a8ee3a80ccc9409d919cdd367
jtaraya <jacobtaraya@gmail.com>
2022-06-29 09:40:14 +0300

    key validator

--- src/main/java/com/jtaraya/util/Validator.java
+++ src/main/java/com/jtaraya/util/Validator.java
@@ -4,18 +4,17 @@ public class Validator {
     // have three methods.
     // note with public access modifiers
     public boolean operationValidator(String operation){
-        return true;
+
         //checks if operation is encode or decode.
         // if its valid  - operation = encode
-        boolean isValid = operation.equalsIgnoreCase( String:"encode") ||
-                            operation.equalsIgnoreCase(String: "decode" );
+        return operation.equalsIgnoreCase(String:"encode") ||
+                operation.equalsIgnoreCase(String: "decode" );
     }
         public boolean messageValidator(String message){
-            boolean isBlankOrEmpty = message.isBlank() && message.isEmpty();
-            return true;
+            return ! (message.isBlank() && message.isEmpty());
             //to check that it's not empty for message
         }
         public boolean keyValidator(int key) {
-                return true;
-            }
+            return(key >= 1) && (key <= 25);
+        }
 }
