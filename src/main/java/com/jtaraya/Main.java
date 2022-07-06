7e5471639c5c096918819f4360c639aa85f350f6
jtaraya <jacobtaraya@gmail.com>
2022-07-06 22:27:36 +0300

    scanner in main.java

--- src/main/java/com/jtaraya/Main.java
+++ src/main/java/com/jtaraya/Main.java
@@ -1,8 +1,34 @@
 package com.jtaraya;
 
+import java.util.Scanner;
+
 public class Main {
     // create three packages. utilities, models and services to group files.
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        String operation;
+        String message;
+        int key;
+
+        System.out.println("Choose the operation (encode || decode)");
+        operation = scanner.nextLine();
+        System.out.println("");
+
+        System.out.println("Enter your message: ");
+        message = scanner.nextLine();
+        System.out.println("");
+
+        System.out.println("Enter the key: ");
+        key = scanner.nextInt();
+        System.out.println("");
+
+
+
+
+
+
+        scanner.close();
+
     }
 }
