9de89237b96fa012712c42390f197c0b60c6c0ff
jtaraya <jacobtaraya@gmail.com>
2022-08-02 20:06:17 +0300

    textClasses output

---
+++ src/test/java/com/jtaraya/services/CaesarCipherTest.java
@@ -0,0 +1,25 @@
+package com.jtaraya.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarCipherTest {
+
+    @Test
+    void testCipherEncodingReturnCorrectCipherText_True() {
+        String plainText = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        String cipherText = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+        int key = 23;
+
+        CaesarCipher caesarCipher = new CaesarCipher();
+        String output = caesarCipher.encode(plainText, key);
+        System.out.println("Text output: " + output);
+
+        assertEquals(cipherText, output);
+    }
+
+    @Test
+    void decode() {
+    }
+}
