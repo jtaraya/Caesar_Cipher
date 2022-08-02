b7a829b95176aaa6694e0b40d7bf308362eb22b4
jtaraya <jacobtaraya@gmail.com>
2022-08-02 18:01:48 +0300

    cipher if else if else

--- src/main/java/com/jtaraya/services/CaesarCipher.java
+++ src/main/java/com/jtaraya/services/CaesarCipher.java
@@ -8,7 +8,28 @@ public class CaesarCipher {
 
 
 
-    public String encode() {
+    public char encode(char c , int key) {
+        int oldIndex = ALPHABET.indexOf(c);
+        int newIndex = (oldIndex + key) % 26;
+
+        int pos;
+        char cipherChar;
+
+        if(newIndex > 25) {
+            pos = newIndex - 26;
+            cipherChar = ALPHABET.charAt(pos);
+
+        } else if (newIndex < 0) {
+            pos = newIndex + 26;
+            cipherChar = ALPHABET.charAt(pos)
+
+        } else{
+            pos = newIndex;
+            cipherChar = ALPHABET.charAt(pos);
+
+        return cipherChar;
+
+        }
         // TODO takes plain text as input and returns cipher text
         return "";
 
