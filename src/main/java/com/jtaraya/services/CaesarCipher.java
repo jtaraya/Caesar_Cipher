299993e8b3bdfbc67e18755f2e611941f7edf695
jtaraya <jacobtaraya@gmail.com>
2022-08-02 19:22:13 +0300

    StringBuilder

--- src/main/java/com/jtaraya/services/CaesarCipher.java
+++ src/main/java/com/jtaraya/services/CaesarCipher.java
@@ -7,36 +7,45 @@ public class CaesarCipher {
     //decode -> newIndex = (oldIndex - key ) % 26; if newIndex < 0 then add 26
 
 
+    public String encode(String message, int key) {
+        char[] messageArray = message.toCharArray();
 
-    public char encode(char c , int key) {
-        int oldIndex = ALPHABET.indexOf(c);
-        int newIndex = (oldIndex + key) % 26;
+        StringBuilder output = new StringBuilder();
 
-        int pos;
-        char cipherChar;
-
-        if(newIndex > 25) {
-            pos = newIndex - 26;
-            cipherChar = ALPHABET.charAt(pos);
-
-        } else if (newIndex < 0) {
-            pos = newIndex + 26;
-            cipherChar = ALPHABET.charAt(pos)
-
-        } else{
-            pos = newIndex;
-            cipherChar = ALPHABET.charAt(pos);
-
-        return cipherChar;
-
+        for (char item : messageArray) {
+            char temp = shiftChar(item, key);
+            output = new StringBuilder(output.append(temp));
         }
-        // TODO takes plain text as input and returns cipher text
-        return "";
 
+
+        return message;
     }
 
     public String decode() {
         // TODO takes cipher text as input and returns plain text
         return "";
     }
+
+    private char shiftChar(char c, int k) {
+        int oldIndex = ALPHABET.indexOf(c);
+        int newIndex = (oldIndex + k) % 26;
+
+        int pos;
+        char cipherChar;
+
+        if (newIndex > 25) {
+            pos = newIndex - 26;
+            cipherChar = ALPHABET.charAt(pos);
+
+        } else if (newIndex < 0) {
+            pos = newIndex + 26;
+            cipherChar = ALPHABET.charAt(pos);
+
+        } else {
+            pos = newIndex;
+            cipherChar = ALPHABET.charAt(pos);
+        }
+        return cipherChar;
+    }
+
 }
